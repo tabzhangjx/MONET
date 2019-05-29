9e07d12f5c734770d13c9ca451e95153e7a519bb
tabzhangjx <tabzhangjx@outlook.com>
2019-05-29 14:40:57 -0500

    a

--- Epi_Semisupervised.py
+++ Epi_Semisupervised.py
@@ -249,7 +249,7 @@ def main():
     num_of_joints = 19
     num_of_limbs = 1
     nBatch_supervised = 1
-    nBatch_unsupervised = 1
+    nBatch_unsupervised = 2
     heatmap_extension_length = 20
     training_iterations = 10000000
 
